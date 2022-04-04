7d3597bac6d2144c36c50fb6c109df601950b760
xavier dharmaiyan <xavier@redhat.com>
2022-04-04 09:39:02 -0400

    fixed lint error
    Signed-off-by: xavier dharmaiyan <xavier@redhat.com>

--- main.go
+++ main.go
@@ -115,7 +115,12 @@ func main() {
 		setupLog.Error(err, "unable to create a new  addon manager", "controller", "SearchOperator")
 	} else {
 		setupLog.Info("starting search addon manager")
-		go addonMgr.Start(ctx)
+		go func() {
+			err := addonMgr.Start(ctx)
+			if err != nil {
+				setupLog.Error(err, "unable to start a new  addon manager", "controller", "SearchOperator")
+			}
+		}()
 	}
 	setupLog.Info("starting manager")
 	if err := mgr.Start(ctx); err != nil {
