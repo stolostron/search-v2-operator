768d958b422879674d3eee9b9c42dff15323496e
Sherin Varughese <shvarugh@redhat.com>
2022-08-09 23:37:59 -0400

    Add table trigger for intercluster edge building (#60)
    * add trigger
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>
    
    * remove extra line
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>
    
    * drop trigger if exists
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>
    
    * add index on edges
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>
    
    * update cluster service version csv
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>
    
    * add triggers
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>
    
    * add impersonate user permission
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>
    
    * update PR image
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>
    
    * add trigger from file
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>
    
    * create if not exists
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>
    
    * remove sql file
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>
    
    * avoid localPlacement subscriptions
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>
    
    * update snapshots
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>
    
    * update snapshots operator
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>
    
    * remove raise notice
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>
    
    * interCluster as edgetype
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>
    
    * update manifest
    
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>

--- controllers/create_pgconfigmap.go
+++ controllers/create_pgconfigmap.go
@@ -18,6 +18,7 @@ func (r *SearchReconciler) PostgresConfigmap(instance *searchv1alpha1.Search) *c
 			Namespace: ns,
 		},
 	}
+
 	data := map[string]string{}
 	data["postgresql.conf"] = `
 ssl = 'on'
@@ -31,9 +32,14 @@ psql -d search -U searchuser -c "CREATE TABLE IF NOT EXISTS search.edges (source
 psql -d search -U searchuser -c "CREATE INDEX IF NOT EXISTS data_kind_idx ON search.resources USING GIN ((data -> 'kind'))"
 psql -d search -U searchuser -c "CREATE INDEX IF NOT EXISTS data_namespace_idx ON search.resources USING GIN ((data -> 'namespace'))"
 psql -d search -U searchuser -c "CREATE INDEX IF NOT EXISTS data_name_idx ON search.resources USING GIN ((data ->  'name'))"
-psql -d search -U searchuser -c "CREATE or REPLACE VIEW search.all_edges AS SELECT * from search.edges UNION SELECT a.uid as sourceid , a.data->>'kind' as sourcekind, b.uid as destid, b.data->>'kind' as destkind, 'deployedBy' as edgetype, a.cluster as cluster FROM search.resources a INNER JOIN search.resources b ON split_part(a.data->>'_hostingSubscription', '/', 1) = b.data->>'namespace' AND split_part(a.data->>'_hostingSubscription', '/', 2) = b.data->>'name' WHERE a.data->>'kind' = 'Subscription' AND b.data->>'kind' = 'Subscription' AND a.uid <> b.uid"`
+psql -d search -U searchuser -c "CREATE INDEX IF NOT EXISTS edges_sourceid_idx ON search.edges USING btree (sourceid)"
+psql -d search -U searchuser -c "CREATE INDEX IF NOT EXISTS edges_destid_idx ON search.edges USING btree (destid)"
+psql -d search -U searchuser -f /opt/app-root/src/postgresql-start/postgresql.sql`
 
+	data["postgresql.sql"] = `
+	CREATE OR REPLACE FUNCTION search.intercluster_edges() RETURNS TRIGGER AS $BODY$ BEGIN if(TG_OP = 'UPDATE') then if coalesce(NEW.data->>'_hostingSubscription','') <> coalesce(OLD.data->>'_hostingSubscription','') then DELETE FROM search.edges where sourceid=OLD.uid OR destid=OLD.uid and edgetype='interCluster'; end if; end if; if(TG_OP = 'INSERT') or (TG_OP = 'UPDATE') then if NEW.data->>'_hostingSubscription' is not null then INSERT INTO search.edges(sourceid ,sourcekind,destid ,destkind ,edgetype ,cluster) SELECT NEW.uid AS sourceid, NEW.data ->> 'kind'::text AS sourcekind, res.uid AS destid, res.data ->> 'kind'::text AS destkind,'interCluster'::text AS edgetype, NEW.cluster from search.resources res where data->>'kind' = 'Subscription' and NEW.data->>'_hostingSubscription' is not null and split_part(NEW.data ->> '_hostingSubscription'::text, '/'::text, 1) = res.data->>'namespace' and split_part(NEW.data ->> '_hostingSubscription'::text, '/'::text, 2) = res.data->>'name' and res.uid <> NEW.uid and res.cluster <> NEW.cluster and res.data ->> '_hostingSubscription' IS NULL ON CONFLICT (sourceid, destid, edgetype) DO NOTHING; elsif NEW.data->>'_hostingSubscription' is null then INSERT INTO search.edges(sourceid ,sourcekind,destid ,destkind ,edgetype ,cluster) SELECT res.uid AS sourceid, res.data ->> 'kind'::text AS sourcekind, NEW.uid AS destid, NEW.data ->> 'kind'::text AS destkind, 'interCluster'::text AS edgetype, res.cluster from search.resources res where res.data->>'kind' = 'Subscription' and split_part(res.data ->> '_hostingSubscription'::text, '/'::text, 1) = NEW.data->>'namespace' and split_part(res.data ->> '_hostingSubscription'::text, '/'::text, 2) = NEW.data->>'name' and res.uid <> NEW.uid and res.cluster <> NEW.cluster ON CONFLICT (sourceid, destid, edgetype) DO NOTHING; end if; RETURN NEW; elsif(TG_OP = 'DELETE') then DELETE FROM search.edges where sourceid=OLD.uid OR destid=OLD.uid and edgetype='interCluster'; RETURN OLD; end if;END; $BODY$ language plpgsql; DROP TRIGGER IF EXISTS resources_upsert on search.resources; CREATE TRIGGER resources_upsert AFTER INSERT OR UPDATE ON search.resources FOR EACH ROW WHEN (NEW.data->>'kind' = 'Subscription') EXECUTE PROCEDURE search.intercluster_edges(); DROP TRIGGER IF EXISTS resources_delete on search.resources; CREATE TRIGGER resources_delete AFTER DELETE ON search.resources FOR EACH ROW WHEN (OLD.data->>'kind' = 'Subscription') EXECUTE PROCEDURE search.intercluster_edges();`
 	cm.Data = data
+	log.Info("configmap data populated")
 
 	err := controllerutil.SetControllerReference(instance, cm, r.Scheme)
 	if err != nil {
