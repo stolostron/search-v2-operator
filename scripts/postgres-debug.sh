1f806f4f5280d3c16c504ec5eb94fec8f20097c4
Jorge Padilla <jpadilla@redhat.com>
2023-06-21 13:36:10 -0400

    Collect inxed usage, vacuum stats. Update several existing queries
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- scripts/postgres-debug.sh
+++ scripts/postgres-debug.sh
@@ -2,13 +2,27 @@
 
 # HOW TO RUN THIS SCRIPT:
 # oc login ... <RHACM Hub>
-# oc exec -it search-postgres-xxxxx -n open-cluster-management -- /bin/bash < postgres-debug.sh
+# oc exec -it search-postgres-xxxx-xxxx -n open-cluster-management -- /bin/bash < postgres-debug.sh
 
 # DESCRIPTION:
-# This script collects data from the Postgres instance usage by RHACM to debug issues with the search service.
+# This script collects data from the Postgres instance to help debug issues with the RHACM search service.
 
 # DATA COLLECTED:
-# 1. POSTGRESQL DEBUG DATA
+# 1. Resources Statistics:
+#    - Total Resource count
+#    - Total Cluster count
+#    - Resource count by cluster
+#    - Resource count by apigroup and kind (top 25)
+#    - Namespaces count by cluster
+#    - Resources by namespace (average, min, max)
+#    - Kubernetes Nodes, CPU, and Memory by cluster
+# 2. Edges Statistics:
+#    - Total Edge count
+#    - Edge count by cluster
+#    - Edges by edge type
+#    - Intercluster edge count by cluster (hub to cluster)
+#    - Total Inter-cluster edges count
+# 3. POSTGRESQL DEBUG DATA
 #    - POSTGRESQL configuration
 #    - POSTGRESQL database size
 #    - POSTGRESQL resources table size
@@ -17,72 +31,38 @@
 #    - POSTGRESQL running queries
 #    - POSTGRESQL idle queries
 #    - POSTGRESQL index usage
-# 2. RHACM Resource Statistics:
-#    - Total Resource count
-#    - Total Cluster count
-#    - Resource count by cluster
-#    - Resource count by kind
-#    - Namespaces count by cluster
-#    - Resource count per cluster/namespace
-#    - Kubernetes node count by cluster
-# 3. RHACM Edges Statistics:
-#    - Total Edge count
-#    - Edge count by cluster
-#    - Edges by edge type
-#    - Intercluster edge count by cluster (hub to cluster)
-#    - Total Inter-cluster edges count
+#    - POSTGRESQL vacuum and table stats
 
-
-printf "\n----- COLLECTING DEBUG DATA FROM POSTGRESQL -----\n"
-
-printf "\n>>> POSTGRESQL Configuration:\n\n"
-psql -d search -U searchuser -c "SHOW all;"
-
-printf "\n>>> POSTGRESQL Database size:\n\n"
-psql -d search -U searchuser -c "SELECT datname, pg_size_pretty(pg_database_size(datname)) from pg_database order by pg_database_size(datname) desc;"
-
-printf "\n>>> POSTGRESQL resources table size:\n\n"
-psql -d search -U searchuser -c "SELECT pg_size_pretty(pg_total_relation_size('search.resources'));"
-
-printf "\n>>> POSTGRESQL edges table size:\n\n"
-psql -d search -U searchuser -c "SELECT pg_size_pretty(pg_total_relation_size('search.edges'));"
-
-printf "\n>>> POSTGRESQL Query activity by state:\n\n"
-psql -d search -U searchuser -c "SELECT count(*),state FROM pg_stat_activity group by state;"
-
-printf "\n>>> POSTGRESQL Running queries:\n\n"
-psql -d search -U searchuser -c "SELECT pid, age(clock_timestamp(), query_start), usename, query FROM pg_stat_activity WHERE state != 'idle' AND query NOT ILIKE '%pg_stat_activity%'  ORDER BY query_start desc;"
-
-printf "\n>>> POSTGRESQL Idle queries:\n\n"
-psql -d search -U searchuser -c "SELECT pid, age(clock_timestamp(), query_start), usename, query FROM pg_stat_activity WHERE state = 'idle' AND query NOT ILIKE '%pg_stat_activity%'  ORDER BY query_start desc;"
-
-printf "\n>>> POSTGRESQL Index usage:\n\n"
-psql -d search -U searchuser -c "SELECT relname, 100 * idx_scan / (seq_scan + idx_scan) percent_of_times_index_used, n_live_tup rows_in_table
-FROM pg_stat_user_tables ORDER BY n_live_tup DESC;"
-
+psql -d search -U searchuser -c "SELECT NOW() as script_start_time;"
+psql -d search -U searchuser -c "SELECT version();"
 
 printf "\n\n----- COLLECTING RESOURCE STATS -----\n"
 
 printf "\n>>> Total resource count:\n\n"
 psql -d search -U searchuser -c "SELECT count(*) FROM search.resources;"
 
-printf "\n>>> Total managed cluster count:\n\n"
+printf "\n>>> Managed cluster count:\n\n"
 psql -d search -U searchuser -c "SELECT count(DISTINCT cluster) FROM search.resources;"
 
 printf "\n>>> Resource count by cluster:\n\n"
 psql -d search -U searchuser -c "SELECT cluster, count(uid) FROM search.resources GROUP BY cluster ORDER BY count DESC;"
 
-printf "\n>>> Resource count by kind:\n\n"
-psql -d search -U searchuser -c "SELECT data->>'apigroup' as apigroup, data->>'kind' as kind, count(uid) FROM search.resources GROUP BY apigroup,kind ORDER BY count DESC;"
+printf "\n>>> Resource count by apigroup and kind (top 25):\n\n"
+psql -d search -U searchuser -c "SELECT data->>'apigroup' as apigroup, data->>'kind' as kind, count(uid)
+    FROM search.resources GROUP BY apigroup,kind ORDER BY count DESC LIMIT 25;"
 
-printf "\n>>> Namespace count by cluster:\n\n"
-psql -d search -U searchuser -c "SELECT cluster, count(DISTINCT data->'namespace') FROM search.resources GROUP BY cluster ORDER BY count DESC;"
+printf "\n>>> Namespace count by managed cluster:\n\n"
+psql -d search -U searchuser -c "SELECT cluster, count(DISTINCT data->'namespace')
+    FROM search.resources GROUP BY cluster ORDER BY count DESC;"
+
+printf "\n>>> Resources by namespace (average, min, max):\n\n"
+psql -d search -U searchuser -c "SELECT avg(count) as avg, min(count) as min, max(count) as max
+    FROM (SELECT cluster, data->>'namespace' as namespace, count(uid)
+    FROM search.resources WHERE data->>'namespace' is not null GROUP BY cluster, namespace) as counts;"
 
 printf "\n>>> Kubernetes Nodes, CPU, and Memory by cluster:\n\n"
-psql -d search -U searchuser -c "SELECT cluster, data->'nodes' as nodes, data->'cpu' as cpu, data->>'memory' as memory FROM search.resources WHERE data->>'kind' = 'Cluster' ORDER BY nodes DESC;"
-
-printf "\n>>> Resource count by cluster and namespace:\n\n"
-psql -d search -U searchuser -c "SELECT cluster, data->>'namespace' as namespace, count(uid) FROM search.resources GROUP BY cluster, namespace ORDER BY cluster, count DESC;"
+psql -d search -U searchuser -c "SELECT cluster, data->'nodes' AS nodes, data->'cpu' AS cpu, data->>'memory' AS memory
+    FROM search.resources WHERE data->>'kind' = 'Cluster' ORDER BY nodes DESC;"
 
 
 printf "\n\n----- COLLECTING EDGES STATS -----\n"
@@ -101,3 +81,51 @@ psql -d search -U searchuser -c "SELECT cluster, count(*) FROM search.edges WHER
 
 printf "/n>>> Total intercluster edge count:\n\n"
 psql -d search -U searchuser -c "SELECT count(*) FROM search.edges WHERE edgetype = 'intercluster';"
+
+
+printf "\n----- COLLECTING DEBUG DATA FROM POSTGRESQL -----\n"
+
+printf "\n>>> POSTGRESQL Configuration:\n\n"
+psql -d search -U searchuser -c "SHOW all;"
+
+printf "\n>>> POSTGRESQL Database size:\n\n"
+psql -d search -U searchuser -c "SELECT datname, pg_size_pretty(pg_database_size(datname))
+    FROM pg_database order by pg_database_size(datname) desc;"
+
+printf "\n>>> POSTGRESQL resources table size:\n\n"
+psql -d search -U searchuser -c "SELECT pg_size_pretty(pg_total_relation_size('search.resources'));"
+
+printf "\n>>> POSTGRESQL edges table size:\n\n"
+psql -d search -U searchuser -c "SELECT pg_size_pretty(pg_total_relation_size('search.edges'));"
+
+printf "\n>>> POSTGRESQL Query activity by state:\n\n"
+psql -d search -U searchuser -c "SELECT count(*),state FROM pg_stat_activity group by state;"
+
+printf "\n>>> POSTGRESQL Running queries:\n\n"
+psql -d search -U searchuser -c "SELECT pid, age(clock_timestamp(), query_start), usename, query
+    FROM pg_stat_activity
+    WHERE state != 'idle' AND query NOT ILIKE '%pg_stat_activity%' ORDER BY query_start desc;"
+
+printf "\n>>> POSTGRESQL Idle queries:\n\n"
+psql -d search -U searchuser -c "SELECT pid, age(clock_timestamp(), query_start), usename, query 
+    FROM pg_stat_activity
+    WHERE state = 'idle' AND query NOT ILIKE '%pg_stat_activity%'
+    ORDER BY query_start desc;"
+
+printf "\n>>> POSTGRESQL Index usage by table:\n\n"
+psql -d search -U searchuser -c "SELECT relname, 100 * idx_scan / (seq_scan + idx_scan) percent_of_times_index_used, n_live_tup rows_in_table
+    FROM pg_stat_user_tables ORDER BY n_live_tup DESC;"
+
+printf "\n>>> POSTGRESQL Index usage:\n\n"
+psql -d search -U searchuser -c "SELECT t.relname AS table_name, i.indexrelname AS index_name, t.seq_scan AS table_scans,
+    i.idx_scan AS index_scans, round(i.idx_scan::numeric / (i.idx_scan + t.seq_scan) * 100, 2) AS index_scan_percentage
+    FROM pg_stat_user_tables t JOIN pg_stat_user_indexes i ON t.relid = i.relid
+    WHERE t.schemaname = 'search' AND t.seq_scan + i.idx_scan > 0
+    ORDER BY index_scan_percentage DESC;"
+
+printf "\n>>> POSTGRESQL Vacuum and table stats:\n\n"
+psql -d search -U searchuser -c "SELECT relname,n_dead_tup,n_live_tup,last_vacuum,last_analyze,last_autovacuum,last_autoanalyze 
+    FROM pg_stat_all_tables 
+    WHERE schemaname = 'search';"
+
+psql -d search -U searchuser -c "SELECT NOW() as script_end_time;"
