479f278c89bd8f9930d88129f8330286a94436f8
Mikela Jackson <mdockery@redhat.com>
2024-04-16 10:11:36 -0400

    Update search_types.go
    starting on updates
    
    Update search_types.go
    
    completing review for API descriptions
    
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- api/v1alpha1/search_types.go
+++ api/v1alpha1/search_types.go
@@ -12,9 +12,9 @@ import (
 type AvailabilityType string
 
 const (
-	// HABasic stands up most app subscriptions with a replicaCount of 1
+	// HABasic creates application subscriptions with a replicaCount of 1.
 	HABasic AvailabilityType = "Basic"
-	// HAHigh stands up most app subscriptions with a replicaCount of 2
+	// HAHigh creates application subscriptions with a replicaCount of 2.
 	// Not supported for development preview.
 	HAHigh AvailabilityType = "High"
 )
@@ -22,7 +22,7 @@ const (
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
 
-// Search is the Schema for the searches API
+// Search is the schema for the searches API.
 type Search struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -31,21 +31,21 @@ type Search struct {
 	Status SearchStatus `json:"status,omitempty"`
 }
 
-// SearchSpec defines the desired state of Search
+// SearchSpec defines the desired state of Search.
 type SearchSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// You can insert additional specification fields.
+	// Important: To regenerate code after you modify the file, run the "make" command.
 
 	// +optional
 	// Storage configuration for the database.
 	DBStorage StorageSpec `json:"dbStorage,omitempty"`
 
 	// +optional
-	// Configmap name contains parameters to override default db parameters
+	// The config map name contains parameters to override default database parameters.
 	DBConfig string `json:"dbConfig,omitempty"`
 
 	// +optional
-	// Customization for search deployments
+	// Customization for search deployments.
 	Deployments SearchDeployments `json:"deployments,omitempty"`
 
 	// +optional
@@ -67,40 +67,40 @@ type SearchSpec struct {
 	ImagePullPolicy corev1.PullPolicy `json:"imagePullPolicy,omitempty"`
 
 	// +optional
-	// NodeSelector to schedule on nodes with matching labels
+	// Define the nodes that you want to achedule with matching labels.
 	NodeSelector map[string]string `json:"nodeSelector,omitempty"`
 
 	// +optional
-	// Tolerations to schedule pods on nodes with matching taints
+	// Define tolerations to schedule pods on nodes with matching taints.
 	Tolerations []corev1.Toleration `json:"tolerations,omitempty"`
 }
 
 type SearchDeployments struct {
 	// +optional
-	// Configuration for DB Deployment
+	// Configuration for the database.
 	Database DeploymentConfig `json:"database,omitempty"`
 
 	// +optional
-	// Configuration for indexer Deployment
+	// Configuration for the indexer.
 	Indexer DeploymentConfig `json:"indexer,omitempty"`
 
 	// +optional
-	// Configuration for collector Deployment
+	// Configuration for the collector.
 	Collector DeploymentConfig `json:"collector,omitempty"`
 
 	// +optional
-	// Configuration for api Deployment
+	// Configuration for Query API.
 	QueryAPI DeploymentConfig `json:"queryapi,omitempty"`
 }
 
 type DeploymentConfig struct {
 	// +optional
 	// +kubebuilder:validation:Minimum:=1
-	// Number of pod instances for deployment
+	// Number of pod instances for the deployment.
 	ReplicaCount int32 `json:"replicaCount,omitempty"`
 
 	// +optional
-	// Compute Resources required by deployment
+	// Compute resources required by deployment.
 	Resources *corev1.ResourceRequirements `json:"resources,omitempty"`
 
 	// +optional
@@ -124,10 +124,10 @@ type StorageSpec struct {
 	Size *resource.Quantity `json:"size,omitempty"`
 }
 
-// SearchStatus defines the observed state of Search
+// SearchStatus defines the observed state of Search.
 type SearchStatus struct {
 
-	// Database used by search
+	// Database used by search.
 	DB string `json:"db"`
 
 	// Storage used by database
@@ -140,7 +140,7 @@ type SearchStatus struct {
 
 // +kubebuilder:object:root=true
 
-// SearchList contains a list of Search
+// SearchList contains a list of Search.
 type SearchList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
