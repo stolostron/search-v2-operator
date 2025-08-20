dc977d67e21eac30c036e37a5bd4392e2203e269
Jorge Padilla <jpadilla@redhat.com>
2025-08-20 12:35:52 -0400

    Update/merge postgres ConfigMap
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- controllers/create_pgconfigmap.go
+++ controllers/create_pgconfigmap.go
@@ -27,7 +27,9 @@ ssl_cert_file = '/sslcert/tls.crt'
 ssl_key_file = '/sslcert/tls.key'
 ssl_ciphers = 'HIGH:!aNULL'
 max_parallel_workers_per_gather = '8'
-statement_timeout = '60000'`
+statement_timeout = '30000'`
+
+	data["additional-postgresql.conf"] = ""
 
 	data[startScript] = `psql -d search -U searchuser -c "CREATE SCHEMA IF NOT EXISTS search"
 psql -d search -U searchuser -c "CREATE TABLE IF NOT EXISTS search.resources (uid TEXT PRIMARY KEY, cluster TEXT, data JSONB)"
