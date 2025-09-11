764b0897c40fe1cd9eac93ace0c818efd6308758
Spencer McAvey <50082193+smcavey@users.noreply.github.com>
2025-09-11 14:58:01 -0400

    ACM-22991 - handle old postgres versions (#663)
    * [WIP] handle old postgres versions
    
    Signed-off-by: Spencer McAvey <smcavey@redhat.com>
    
    * grab install images postgres --version to compare current vs install
    
    Signed-off-by: Spencer McAvey <smcavey@redhat.com>
    
    * utilize postgresql-pre-start hook
    
    Signed-off-by: Spencer McAvey <smcavey@redhat.com>
    
    * remove before-start.sh
    
    Signed-off-by: Spencer McAvey <smcavey@redhat.com>
    
    * add postgresql-pre-start volumemount sub path to only copy relevant script
    
    Signed-off-by: Spencer McAvey <smcavey@redhat.com>
    
    * correct pre-start volume and mountpath
    
    Signed-off-by: Spencer McAvey <smcavey@redhat.com>
    
    * add comment regarding why its safe to delete data
    
    Signed-off-by: Spencer McAvey <smcavey@redhat.com>
    
    ---------
    
    Signed-off-by: Spencer McAvey <smcavey@redhat.com>

--- controllers/create_pgdeployment.go
+++ controllers/create_pgdeployment.go
@@ -47,6 +47,11 @@ func (r *SearchReconciler) PGDeployment(instance *searchv1alpha1.Search) *appsv1
 				MountPath: "/opt/app-root/src/postgresql-cfg",
 			},
 			{
+				Name:      "postgresql-pre-start",
+				MountPath: "/opt/app-root/src/postgresql-pre-start/postgresql-pre-start.sh",
+				SubPath:   "postgresql-pre-start.sh",
+			},
+			{
 				Name:      "postgresql-start",
 				MountPath: "/opt/app-root/src/postgresql-start",
 			},
@@ -103,6 +108,16 @@ func (r *SearchReconciler) PGDeployment(instance *searchv1alpha1.Search) *appsv1
 	volumes := []corev1.Volume{
 		{
 			Name: "postgresql-cfg",
+			VolumeSource: corev1.VolumeSource{
+				ConfigMap: &corev1.ConfigMapVolumeSource{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: postgresConfigmapName,
+					},
+				},
+			},
+		},
+		{
+			Name: "postgresql-pre-start",
 			VolumeSource: corev1.VolumeSource{
 				ConfigMap: &corev1.ConfigMapVolumeSource{
 					LocalObjectReference: corev1.LocalObjectReference{
