3c084c1aeed609ae06fb2594a5156b2ddcfba602
Jorge Padilla <jpadilla@redhat.com>
2026-01-22 14:41:02 -0500

    Update test
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- controllers/fine_grained_rbac_test.go
+++ controllers/fine_grained_rbac_test.go
@@ -49,7 +49,7 @@ func Test_disableFineGrainedRBAC(t *testing.T) {
 			Name:      "search-operator",
 			Namespace: "open-cluster-management",
 			Annotations: map[string]string{
-				"fine-grained-rbac-preview": "false",
+				"fine-grained-rbac": "false",
 			},
 		},
 		Spec: searchv1alpha1.SearchSpec{},
